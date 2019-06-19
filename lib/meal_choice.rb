4c3522cb9d206b5665fa413d7a58a129b4a96d7b
Randall Taylor <randyisbusy@gmail.com>
2019-06-19 20:44:37 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,6 @@
 # Your code here
 def meal_choice(veg1, veg2, protein = "meat")
-  sen = "A plate of #{protein}, with #{veg1}, and #{vet2}."
+  sen = "A plate of #{protein}, with #{veg1}, and #{veg2}."
   puts sen
   return sen
 end
